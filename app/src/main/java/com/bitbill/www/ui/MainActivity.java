bdca4553343d3e22b6b0a88a6e92e074945913b9
isanwenyu <isanwenyu@163.com>
2017-11-07 16:10:25 +0800

    custom the CardView

--- app/src/main/java/com/bitbill/www/ui/MainActivity.java
+++ app/src/main/java/com/bitbill/www/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.bitbill.www;
+package com.bitbill.www.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import com.bitbill.www.R;
 
 public class MainActivity extends AppCompatActivity {
 
