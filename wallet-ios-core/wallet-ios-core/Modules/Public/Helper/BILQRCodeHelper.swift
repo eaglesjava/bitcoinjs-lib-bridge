4b897fbb5fe6527d94ec652905167443f065db29
Sniper256 <work_cocody@hotmail.com>
2017-12-25 10:59:17 +0800

    去除重复代码

--- wallet-ios-core/wallet-ios-core/Modules/Public/Helper/BILQRCodeHelper.swift
+++ wallet-ios-core/wallet-ios-core/Modules/Public/Helper/BILQRCodeHelper.swift
@@ -11,20 +11,6 @@ import UIKit
 class BILQRCodeHelper: NSObject {
     static func generateQRCode(msg: String, targetSize: CGSize = CGSize(width: 270, height: 270)) -> UIImage? {
         
-        guard let filter = CIFilter(name: "CIQRCodeGenerator") else {
-            return nil
-        }
-        
-        let data = msg.data(using: .utf8)
-        
-        filter.setValue(data, forKey: "inputMessage")
-        
-        guard let ciImage = filter.outputImage else {
-            return nil
-        }
-        let scale = targetSize.width / ciImage.extent.size.width
-        let magnifiedImage = ciImage.transformed(by: CGAffineTransform(scaleX: scale, y: scale))
-        
-        return UIImage(ciImage: magnifiedImage)
+        return msg.qrCodeImage(targetSize: targetSize)
     }
 }
