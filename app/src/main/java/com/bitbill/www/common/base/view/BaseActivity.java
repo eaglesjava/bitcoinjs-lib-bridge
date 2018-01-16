3a093feca847abd625f5fe1fe0346bbbabf4b71c
isanwenyu <isanwenyu@163.com>
2018-01-16 19:35:40 +0800

    移除测试代码

--- app/src/main/java/com/bitbill/www/common/base/view/BaseActivity.java
+++ app/src/main/java/com/bitbill/www/common/base/view/BaseActivity.java
@@ -15,7 +15,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -79,7 +78,6 @@ public abstract class BaseActivity<P extends MvpPresenter> extends AppCompatActi
                 }
             }
         }
-        Log.d("BaseActivity", "onCreate: " + StringUtils.getContactKey());
     }
 
     public BitbillApp getApp() {
