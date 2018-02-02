fd519e086fa436a61c2eaa2c174d3ed350660a7b
isanwenyu <isanwenyu@163.com>
2018-02-02 13:32:57 +0800

    二维码扫描添加选择相册图片功能

--- app/src/main/java/com/bitbill/www/ui/main/send/ScanQrcodeActivity.java
+++ app/src/main/java/com/bitbill/www/ui/main/send/ScanQrcodeActivity.java
@@ -1,12 +1,18 @@
 package com.bitbill.www.ui.main.send;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import com.bitbill.www.R;
 import com.bitbill.www.app.AppConstants;
@@ -16,7 +22,9 @@ import com.bitbill.www.common.utils.StringUtils;
 import com.bitbill.www.common.utils.UIHelper;
 
 import butterknife.BindView;
+import cn.bingoogolapple.photopicker.activity.BGAPhotoPickerActivity;
 import cn.bingoogolapple.qrcode.core.QRCodeView;
+import cn.bingoogolapple.qrcode.zxing.QRCodeDecoder;
 
 public class ScanQrcodeActivity extends BaseToolbarActivity implements QRCodeView.Delegate {
 
@@ -142,5 +150,66 @@ public class ScanQrcodeActivity extends BaseToolbarActivity implements QRCodeVie
     @Override
     public void onScanQRCodeOpenCameraError() {
         showMessage(R.string.fail_open_camera);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.select_photo_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_select_photo) {
+            //从相册选择图片
+            Intent photoPickerIntent = new BGAPhotoPickerActivity.IntentBuilder(this)
+                    .cameraFileDir(null)
+                    .maxChooseCount(1)
+                    .selectedPhotos(null)
+                    .pauseOnScroll(false)
+                    .build();
+            startActivityForResult(photoPickerIntent, REQUEST_CODE_CHOOSE_QRCODE_FROM_GALLERY);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        mQRCodeView.showScanRect();
+
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_CHOOSE_QRCODE_FROM_GALLERY) {
+            final String picturePath = BGAPhotoPickerActivity.getSelectedPhotos(data).get(0);
+
+            /*
+            这里为了偷懒，就没有处理匿名 AsyncTask 内部类导致 Activity 泄漏的问题
+            请开发在使用时自行处理匿名内部类导致Activity内存泄漏的问题，处理方式可参考 https://github.com/GeniusVJR/LearningNotes/blob/master/Part1/Android/Android%E5%86%85%E5%AD%98%E6%B3%84%E6%BC%8F%E6%80%BB%E7%BB%93.md
+             */
+            new AsyncTask<Void, Void, String>() {
+                @Override
+                protected String doInBackground(Void... params) {
+                    return QRCodeDecoder.syncDecodeQRCode(picturePath);
+                }
+
+                @Override
+                protected void onPostExecute(String result) {
+                    if (TextUtils.isEmpty(result)) {
+                        Toast.makeText(ScanQrcodeActivity.this, R.string.fail_not_found_qrcode, Toast.LENGTH_SHORT).show();
+                    } else {
+                        onScanQRCodeSuccess(result);
+                    }
+                }
+            }.execute();
+        }
     }
 }
